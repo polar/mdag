82068b2ddd04223ab652dcbbb65a3f33b3fe4e9b
Polar Humenn <polar@adiron.com>
2011-11-05 02:25:14 -0400

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdag::Application.config.secret_token = '5837b59f4e3840246234608c80a394fb579f95db9d6cb9dad978f938a2f18d81b17f549029869a9a296af8af608b3c351be6ec9ada89086a6141bc6e60ce2e77'
